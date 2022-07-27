ce8a87c7ec0d9786fb93c6f5ca0d199c81d8a736
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-07-27 17:45:55 +0100

    Update NoticeBoard.tsx

--- components/NoticeBoard.tsx
+++ components/NoticeBoard.tsx
@@ -4,16 +4,18 @@ import Image from "next/image";
 function NoticeBoard() {
   return (
     <section className="p-1 flex items-center bg-gradient-to-r from-pink-500 to-yellow-500 justify-center  my-10  max-w-4xl mx-auto  border  rounded-md shadow border-zinc-700">
-      <div className="text-center px-3 rounded-md bg-[#1A1A1A] py-5">
+      <div className="text-center px-4 rounded-md bg-[#1A1A1A] py-6">
         {" "}
         <h2 className="text-center text-zinc-200 font-semibold text-2xl mb-3 ">
           Notice Board!!!
         </h2>
-        <p className="mb-8">
+        <p className="mb-2 mt-4">
           Dear Users, I apologize for the downtime witnessed over the week.
           Unfortunately, some files and pages were lost due to some technical
-          issues. I am working hard to get them back . Please bear with me.
+          issues. I am working hard to get them back . In the mean time, some
+          pages and files may not load correctly.
         </p>
+        <p className="mb-8 font-semibold  ">Please bear with me.</p>
         <Image
           src="/images/sad.gif"
           className="rounded-md"
