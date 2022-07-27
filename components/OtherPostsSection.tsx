99520b37c308c60f21c586c27818eec099705bcf
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-07-27 18:49:00 +0100

    Update OtherPostsSection.tsx

--- components/OtherPostsSection.tsx
+++ components/OtherPostsSection.tsx
@@ -28,31 +28,6 @@ function OtherPostsSection({ otherPosts }: any) {
           <OtherPostBlock otherPost={partitionPost} key={index} />
         </div>
       ))}{" "}
-      <div className="flex items-center justify-center gap-x-4">
-        {/* only show 'show more button' whben partitionPost.length is less  than otherPosts.length  */}
-        {partitionPosts.length === otherPosts.length ? (
-          <button className="border-2 rounded px-7 py-2 my-10 border-zinc-800 hover:bg-zinc-800 transition ease-in-out font-semibold">
-            THE END😎
-          </button>
-        ) : (
-          <button
-            className=" flex items-center justify-center  rounded px-7 py-2 my-10 text-zinc-800 hover: bg-teal hover:bg-darkTeal transition ease-in-out font-semibold"
-            onClick={() => showMore()}
-          >
-            {/* SHOW LOADING TEXT WHEN BUTTN IS CLICKED */}
-            SHOW MORE <CgPushDown className="ml-2" />
-          </button>
-        )}{" "}
-        {partitionPosts.length === 5 ? null : (
-          <button
-            className=" flex items-center justify-center border-2 rounded px-7 py-2 my-10 border-zinc-800 hover:bg-zinc-800 transition ease-in-out font-semibold"
-            onClick={() => showLess()}
-          >
-            {/* SHOW LOADING TEXT WHEN BUTTN IS CLICKED */}
-            SHOW LESS <CgPushUp className="ml-2" />
-          </button>
-        )}
-      </div>
       <div className="mt-10">
         {" "}
         <Nb type={2} />
