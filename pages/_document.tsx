6ad7b8284bca4e760fc648dc3ac40035a6401374
Chibuike Nnaji <67190735+chinnaji@users.noreply.github.com>
2022-10-20 03:46:50 +0100

    done

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,44 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
+import { GA_TRACKING_ID } from "../helpers/gtag";
+
 function MyDocument() {
+  const isProduction = process.env.NODE_ENV === "production";
+
   return (
     <Html lang="en">
       <Head>
         {/* <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
         <meta name="theme-color" content="#fff" /> */}
-        <Script
+        {/* enable analytics script only for production */}
+        {isProduction && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              strategy="afterInteractive"
+            />
+            <script
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+              }}
+            />
+          </>
+        )}
+        {/* <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-8M0MV4GMY6"
         />
@@ -23,7 +53,7 @@ function MyDocument() {
               gtag('config', 'G-8M0MV4GMY6');
           `,
           }}
-        />
+        /> */}
       </Head>
       <body className="bg-[#111111] relative text-zinc-100">
         <Main />
